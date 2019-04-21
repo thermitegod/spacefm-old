873d24e340b20a9b9db46fbed1c44bd06fa397b6
thermitegod <thermitethegod@gmail.com>
2019-04-20 22:13:45 -0700

    remove lxde compat code

--- src/settings.c
+++ src/settings.c
@@ -48,9 +48,6 @@
 
 #define DEFAULT_TMP_DIR "/tmp"
 
-/* Dirty hack: check whether we are under LXDE or not */
-#define is_under_LXDE()     (g_getenv( "_LXSESSION_PID" ) != NULL)
-
 AppSettings app_settings = {0};
 /* const gboolean singleInstance_default = TRUE; */
 const gboolean show_hidden_files_default = FALSE;
